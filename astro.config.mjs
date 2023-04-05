b007771f9997862a0f45c5aec9dd7326fabd4612
golden05 <golden05@gmail.com>
2023-04-05 09:54:56 +0800

    unocss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
 import { defineConfig } from 'astro/config';
+import unocss from '@unocss/astro';
+import { presetWind } from '@unocss/preset-wind';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    unocss({
+      presets: [
+        presetWind(),
+      ],
+      safelist: [
+        
+      ],
+    }),
+  ]
+});
